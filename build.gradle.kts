a07d3bf2795363b3e9db1dde328425ff7f8d364c
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2023-03-20 19:57:47 +0900

    ⬆️ :: downgrade kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.8.0"
+    kotlin("jvm") version "1.6.0"
     id("maven-publish")
 }
 
@@ -18,7 +18,7 @@ tasks.test {
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+    kotlinOptions.jvmTarget = "1.6"
 }
 
 publishing {
